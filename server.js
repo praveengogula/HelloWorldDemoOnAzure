215ffb85ee45592aef9e37ee5c96afefd0a9ba99
Praveen Kumar Gogula <praveengogula20@gmail.com>
2016-08-09 04:21:41 -0700

    change2

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Mr.Chanakya Lokam, Welcome to Azure Cloud\n');
+  res.end('Hello Mr.Chanakya Lokam from Miracle Software Systems,Inc. Welcome to Azure Cloud\n');
 }).listen(port);
