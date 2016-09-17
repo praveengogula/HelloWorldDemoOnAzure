21afe4da1e90ebd64ce1bcf8eb476abc23610835
Praveen Kumar Gogula <praveengogula20@gmail.com>
2016-09-17 03:26:59 -0400

    Avanthi West-Us web app1

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello All. Welcome to Azure Cloud\n');
+  res.end('Avanthi West-Us web app1 \n');
 }).listen(port);
